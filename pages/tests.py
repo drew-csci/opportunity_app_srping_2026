d5b010c8cc65341fe89488a7b9bb0d456a1b0c1d
modestmag-dev <257359355+modestmag-dev@users.noreply.github.com>
2026-04-08 14:06:18 -0400

    Issue #24: Added first unit test, which is a Happy Path test that aims to see if the correct type of dashboard is outputted for a user of a specific role (in this case, it tests if an organization dashboard loads for that type of user).

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+# Happy path test for loading a user's specific dashboard view based on their user type, also testing if Dashboard
+# opens after login.
+class DashboardHappyPathTests(TestCase):
+    # Sets up mock user account, which in this case, is of the organization user type.
+    def setUp(self):
+        self.User = get_user_model()
+        self.user = self.User.objects.create_user(
+            email="org@example.com",
+            username="org@example.com",
+            password="TestPass123!",
+            user_type="organization",
+            first_name="Org",
+            last_name="Owner",
+        )
+
+    # Tests the above credentials to see if, after implementing them, they lead to the correct dashboard page
+    # meant for the given user type.
+    def test_dashboard_renders_for_logged_in_user(self):
+        self.client.login(username=self.user.email, password="TestPass123!")
+        response = self.client.get(reverse("dashboard"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Organization Dashboard")
+        self.assertContains(response, self.user.display_name)
